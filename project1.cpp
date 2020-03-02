802da5f029f6e3f56c0a74bcbfef8ab257578078
root <root@localhost.localdomain>
2020-03-02 22:48:44 +0000

    created map of Pairs of strings with strings

--- project1.cpp
+++ project1.cpp
@@ -10,30 +10,50 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <map>
+#include <iterator>
 using namespace std;
 
-//struct State;
-//struct Word;
-//void calcProbability(struct Word *word, struct State *state);
+//typedef pair<string,string> strPair;
+
+//void calcProbability();
 
 int main(){
+	//typedef vector<string> vecString;
+	
+	multimap<pair<string,string>, string> markovMMap;
+	//multimap<strPair, string> markovMMap;
 	int i=0;
+	//vector<string> tweetVect;
 	string tweetArr[280];
 	ifstream file;
 	ofstream outFile;
 	file.open("tweet.txt");
-	//getline(file, tweetLine);
+
 	while(file){
 		file >> tweetArr[i];
-		++i;
+		if(i>1){
+			markovMMap.insert(make_pair(make_pair(tweetArr[i-2],tweetArr[i-1]),tweetArr[i]));
+
+		}
+		else if(i=1){
+			markovMMap.insert(make_pair(make_pair("empty",tweetArr[i-1]),tweetArr[i]));
+		
+		}
+		else{
+			markovMMap.insert(make_pair(make_pair("empty","empty"),tweetArr[i]));
+	
+		}
+		i++;
 	}
 
 	outFile.open("tweetOut.txt");
-	int j=0;
-	while(j<=i){
-		outFile<<tweetArr[j]<<" ";
-		
-		++j;
+
+	multimap<pair<string,string>,string>::iterator iter;
+
+	for(iter = markovMMap.begin(); iter != markovMMap.end(); ++iter){
+		outFile << iter->first.first << " " << iter->first.second << " " << iter->second << endl;
 	}
 
 	return 0;
@@ -41,19 +61,6 @@ int main(){
 }
 
 /*
-struct State{
-	char *prevWord;
-	char *currWord;
-	char *nextWord;
-};
-
-struct Word{
-	int totalWords;
-	int thisWord;
-	int probability;
-	//struct State *wordState;
-	char *theCurrentWord;
-};
 
 void calcProbability(struct Word *word, struct State *wordState){
 	word->probability = word->thisWord/word->totalWords;
