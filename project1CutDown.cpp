60a70499b6f73de85c3e48904b4e4fa4b2c5703b
root <root@localhost.localdomain>
2020-03-08 17:43:02 -0400

    protected project1CutDown.cpp

--- project1CutDown.cpp
+++ project1CutDown.cpp
@@ -21,116 +21,133 @@ using namespace std;
 
 int main(){
 	//typedef vector<string> vecString;
-  srand(time(NULL));
+  	srand(time(NULL));
 	//multimap<pair<string,string>, pair<string,double>> markovMMap;
 	multimap<pair<string,string>, string> markovMMap;
 	
 	int i=0;
 	vector<string> tweetVect;
-  string strTweet;
+  	string strTweet;
 	ifstream file;
 	ofstream outFile;
+	
 	file.open("tweet2.txt");
+	if(!file){
+		outFile.open("tweetOut2.txt");
+		outFile << "input file does not exist";
+	}
+	else{
+		while(file >> strTweet){
+			tweetVect.push_back(strTweet);
+			if(i>1){
+				markovMMap.insert(make_pair(make_pair(tweetVect[i-2],tweetVect[i-1]),tweetVect[i]));
 
-	while(file >> strTweet){
-		tweetVect.push_back(strTweet);
-		if(i>1){
-			markovMMap.insert(make_pair(make_pair(tweetVect[i-2],tweetVect[i-1]),tweetVect[i]));
-
-		}
-		else if(i==1){
-			markovMMap.insert(make_pair(make_pair("empty",tweetVect[i-1]),tweetVect[i]));
+			}
+			else if(i==1){
+				markovMMap.insert(make_pair(make_pair("empty",tweetVect[i-1]),tweetVect[i]));
 		
+			}
+			else{
+				markovMMap.insert(make_pair(make_pair("empty","empty"),tweetVect[i]));
+	
+			}
+			i++;
+		}
+
+		//multimap<pair<string,string>,string>::iterator iter1;
+		//multimap<pair<string,string>,string>::iterator iter2;
+	
+		outFile.open("tweetOut2.txt");	
+	
+		multimap<pair<string,string>,string>::iterator iter;
+
+  
+  
+  		//dene's attempt
+  		int charCount = 0;
+  		vector<string> wordTracker; //to keep track of the words placed into our tweet
+  		int wordTrackerIndex = 0;
+		int randIndex;
+  		pair<multimap<pair<string,string>,string>::iterator, multimap<pair<string,string>,string>::iterator> result = markovMMap.equal_range(pair<string,string> ("empty","empty"));
+
+  		vector<string> randProbWord;
+  		i = 0;
+  		for(iter = result.first; iter != result.second; ++iter){
+    			randProbWord.push_back(iter->second);
+    			//cout << randProbWord[i] << endl;
+    			i++;
+  		}	
+  		if(randProbWord.size() == 0){
+			charCount = 281;
 		}
 		else{
-			markovMMap.insert(make_pair(make_pair("empty","empty"),tweetVect[i]));
-	
+			//cout << randProbWord.size() << endl;  
+  			randIndex = rand()%randProbWord.size();
+  			//cout << randIndex << endl;
+  			outFile << randProbWord[randIndex] << " ";
+  			charCount += randProbWord[randIndex].length() + 1; //the plus one is to account for the spaces
+  			cout << charCount << endl;
+  			wordTracker.push_back(randProbWord[randIndex]);
 		}
-		i++;
+  		while(!randProbWord.empty()){
+    			randProbWord.pop_back();
+  		}
+
+  
+  		result = markovMMap.equal_range(pair<string,string> ("empty",wordTracker[0]));
+
+  		i = 0;
+  		for(iter = result.first; iter != result.second; ++iter){
+    			randProbWord.push_back(iter->second);
+    			i++;
+  		}
+		if(randProbWord.size() == 0 || charCount == 281){
+			charCount = 281;
+		}
+		else{
+	  		randIndex = rand()%randProbWord.size();
+  			outFile << randProbWord[randIndex] << " ";
+  			charCount += randProbWord[randIndex].length() + 1; 
+  			cout << charCount << endl;
+  			wordTracker.push_back(randProbWord[randIndex]);
+  			wordTrackerIndex += 1;
+		}
+  		while(!randProbWord.empty()){
+    			randProbWord.pop_back();
+  		}
+  
+  		while(charCount < 280){
+    			result = markovMMap.equal_range(pair<string,string> (wordTracker[wordTrackerIndex-1],wordTracker[wordTrackerIndex])); //previous two words
+
+    			i = 0;
+    			for(iter = result.first; iter != result.second; ++iter){
+      				randProbWord.push_back(iter->second);
+      				i++;
+    			}
+    			if(randProbWord.size() == 0){
+      				charCount = 281; //end the output tweet because the probability that there are no more words has been chosen
+    			}	
+    			else{ 
+      				randIndex = rand()%randProbWord.size();
+      				outFile << randProbWord[randIndex] << " ";
+      				charCount += randProbWord[randIndex].length() + 1; 
+      				cout << charCount << endl;
+      				wordTracker.push_back(randProbWord[randIndex]);
+      				wordTrackerIndex += 1;
+
+      				while(!randProbWord.empty()){
+        				randProbWord.pop_back();
+      				}
+    			}
+  		}
+
+  		while(!randProbWord.empty()){
+			randProbWord.pop_back();
+		}
+		while(!tweetVect.empty()){
+			tweetVect.pop_back();
+		}
 	}
-
-	//multimap<pair<string,string>,string>::iterator iter1;
-	//multimap<pair<string,string>,string>::iterator iter2;
-	
-	outFile.open("tweetOut2.txt");	
-	
-	multimap<pair<string,string>,string>::iterator iter;
-
-  
-  
-  //dene's attempt
-  int charCount = 0;
-  vector<string> wordTracker; //to keep track of the words placed into our tweet
-  int wordTrackerIndex = 0;
-
-  pair<multimap<pair<string,string>,string>::iterator, multimap<pair<string,string>,string>::iterator> result = markovMMap.equal_range(pair<string,string> ("empty","empty"));
-
-  vector<string> randProbWord;
-  i = 0;
-  for(iter = result.first; iter != result.second; ++iter){
-    randProbWord.push_back(iter->second);
-    //cout << randProbWord[i] << endl;
-    i++;
-  }	
-  //cout << randProbWord.size() << endl;  
-  int randIndex = rand()%randProbWord.size();
-  //cout << randIndex << endl;
-  outFile << randProbWord[randIndex] << " ";
-  charCount += randProbWord[randIndex].length() + 1; //the plus one is to account for the spaces
-  cout << charCount << endl;
-  wordTracker.push_back(randProbWord[randIndex]);
-
-  while(!randProbWord.empty()){
-    randProbWord.pop_back();
-  }
-
-  
-  result = markovMMap.equal_range(pair<string,string> ("empty",wordTracker[0]));
-
-  i = 0;
-  for(iter = result.first; iter != result.second; ++iter){
-    randProbWord.push_back(iter->second);
-    i++;
-  }	  
-  randIndex = rand()%randProbWord.size();
-  outFile << randProbWord[randIndex] << " ";
-  charCount += randProbWord[randIndex].length() + 1; 
-  cout << charCount << endl;
-  wordTracker.push_back(randProbWord[randIndex]);
-  wordTrackerIndex += 1;
-
-  while(!randProbWord.empty()){
-    randProbWord.pop_back();
-  }
-  
-  while(charCount < 280){
-    result = markovMMap.equal_range(pair<string,string> (wordTracker[wordTrackerIndex-1],wordTracker[wordTrackerIndex])); //previous two words
-
-    i = 0;
-    for(iter = result.first; iter != result.second; ++iter){
-      randProbWord.push_back(iter->second);
-      i++;
-    }
-    if(randProbWord.size() == 0){
-      charCount = 281; //end the output tweet because the probability that there are no more words has been chosen
-    }	
-    else{ 
-      randIndex = rand()%randProbWord.size();
-      outFile << randProbWord[randIndex] << " ";
-      charCount += randProbWord[randIndex].length() + 1; 
-      cout << charCount << endl;
-      wordTracker.push_back(randProbWord[randIndex]);
-      wordTrackerIndex += 1;
-
-      while(!randProbWord.empty()){
-        randProbWord.pop_back();
-      }
-    }
-    
-  }
-
-  
-
 	return 0;
 
 }
